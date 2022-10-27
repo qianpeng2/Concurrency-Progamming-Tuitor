91799f186a63ef350f4a24f18a065a331f975628
peng.qian <peng.qian@cue.group>
2022-10-27 17:37:00 +0800

    栅栏解释1

--- concurrency/src/main/java/com/javaedge/concurrency/example/aqs/CyclicBarrierExample1.java
+++ concurrency/src/main/java/com/javaedge/concurrency/example/aqs/CyclicBarrierExample1.java
@@ -11,11 +11,25 @@ public class CyclicBarrierExample1 {
 
     private static CyclicBarrier barrier = new CyclicBarrier(5);
 
+    /**
+     * CountDownLatch有几个问题：
+     * 首先CountDownLatch在await之后必须依靠别的线程来给它countDown，打开门闩；
+     * <p>
+     * 其次CountDownLatch在countDown到0之后，该CountDownLatch的生命周期就结束了，它不能重用。
+     * <p>
+     * CyclicBarrier: 回环栅栏
+     * 它可以自己给自己打开门闩(shuan) ，还能重用
+     * 通俗的说，就是有一个栅栏-CyclicBarrier，需要n个人同时推倒，推倒后这n个人继续执行
+     * 然后一旦栅栏被推倒，后面还可以再次竖起来
+     *
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
 
         ExecutorService executor = Executors.newCachedThreadPool();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {// 10，相当于栅栏打开2次，重用了一次，
             final int threadNum = i;
             Thread.sleep(1000);
             executor.execute(() -> {
