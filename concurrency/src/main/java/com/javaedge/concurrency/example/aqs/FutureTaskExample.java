29c3e09479d09ac094da24a7acf5dede8d0c8275
peng.qian <peng.qian@cue.group>
2022-11-01 11:33:58 +0800

    可取消的异步任务

--- concurrency/src/main/java/com/javaedge/concurrency/example/aqs/FutureTaskExample.java
+++ concurrency/src/main/java/com/javaedge/concurrency/example/aqs/FutureTaskExample.java
@@ -8,14 +8,18 @@ import java.util.concurrent.FutureTask;
 @Slf4j
 public class FutureTaskExample {
 
+    /**
+     * FutureTask：可取消的异步任务框架， 线程的执行通过 FutureTask 的方式，而不是递交到线程池中执行
+     * 线程执行完毕，main也就完毕
+     *
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        FutureTask<String> futureTask = new FutureTask<String>(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                log.info("do something in callable");
-                Thread.sleep(5000);
-                return "Done";
-            }
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
+            log.info("do something in callable");
+            Thread.sleep(5000);
+            return "Done";
         });
 
         new Thread(futureTask).start();
