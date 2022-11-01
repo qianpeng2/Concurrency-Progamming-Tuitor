8418d858fb0c4fedee35a79fb0c65f8c1f53a715
peng.qian <peng.qian@cue.group>
2022-11-01 16:06:18 +0800

    线程申请的许可超出初始值

--- concurrency/src/main/java/com/javaedge/concurrency/example/aqs/SemaphoreExample2.java
+++ concurrency/src/main/java/com/javaedge/concurrency/example/aqs/SemaphoreExample2.java
@@ -17,13 +17,18 @@ public class SemaphoreExample2 {
 
         final Semaphore semaphore = new Semaphore(3);
 
-        for (int i = 0; i < threadCount; i++) {
+        for (int i = 1; i <= threadCount; i++) {
             final int threadNum = i;
             exec.execute(() -> {
                 try {
-                    semaphore.acquire(3); // 获取多个许可
+                    log.info("线程{}尝试一次性获取4个许可", threadNum);
+                    // 如果每次尝试的许可数量超过了 初始许可数量，那么所有的线程都尝试获取这么多许可
+                    // 20个线程 全部进入无限的等待
+                    semaphore.acquire(4); // 获取多个许可
                     test(threadNum);
+                    log.info("线程{}尝试一次性释放3个许可", threadNum);
                     semaphore.release(3); // 释放多个许可
+                    log.info("线程{}一次性释放3个许可成功", threadNum);
                 } catch (Exception e) {
                     log.error("exception", e);
                 }
