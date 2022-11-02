1bdc379b577b74a5186f13e899a5e208717d62e9
peng.qian <peng.qian@cue.group>
2022-11-02 18:16:51 +0800

    AtomicReference

--- concurrency/src/main/java/com/javaedge/concurrency/example/atomic/AtomicExample4.java
+++ concurrency/src/main/java/com/javaedge/concurrency/example/atomic/AtomicExample4.java
@@ -9,10 +9,12 @@ import java.util.concurrent.atomic.AtomicReference;
 @ThreadSafe
 public class AtomicExample4 {
 
+    // AtomicReference 不仅仅是封装Integer ，还是可以封装普通对象 Object
     private static AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public static void main(String[] args) {
-        count.compareAndSet(0, 2); // 2
+        // count 初始值为0，compareAndSet：如果期望值和实际值相等，那么更新为update值，否则不变
+        count.compareAndSet(0, 2); // count=2
         count.compareAndSet(0, 1); // no
         count.compareAndSet(1, 3); // no
         count.compareAndSet(2, 4); // 4
