315237eb8d92297320aa66d4e2eb1f51c9c815c5
peng.qian <peng.qian@cue.group>
2022-11-04 16:35:01 +0800

    ReentrantReadWriteLock 问题解决

--- concurrency/src/main/java/com/javaedge/concurrency/example/lock/LockExample3.java
+++ concurrency/src/main/java/com/javaedge/concurrency/example/lock/LockExample3.java
@@ -44,7 +44,7 @@ public class LockExample3 {
             return map.put(key, value);
         } finally {
             log.info("准备释放写锁");
-            readLock.unlock();
+            writeLock.unlock();// 妈的，这里写错了，是写锁的释放而不是读锁的释放
             log.info("写锁释放成功");//问题是这里的写锁无法释放
         }
     }
@@ -62,36 +62,37 @@ public class LockExample3 {
 
         // 改了之后这个主线程居然不会停止
         ExecutorService executorService = Executors.newCachedThreadPool();
-//        final Semaphore semaphore = new Semaphore(1);
-//        CountDownLatch countDownLatch = new CountDownLatch(10);
+        final Semaphore semaphore = new Semaphore(5);
+        int count = 10;// 在10个线程并发的情况下，正常，一旦线程数超过了100，那么还是容易出现并发的报错
+        CountDownLatch countDownLatch = new CountDownLatch(count);
         LockExample3 lockExample3 = new LockExample3();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < count; i++) {
             final String num = String.valueOf(i);
             executorService.submit(() -> {// 用 submit 不会报错，但是用 execute 会报错
-//                try {
-                /**
-                 * //如果没有的话，那么报错为: java.lang.IllegalMonitorStateException:
-                 * attempt to unlock read lock, not locked by current thread
-                 *
-                 * 就是同时做读写操作的时候，很容易出现问题，如果是只读则没关系
-                 */
-                log.info("线程{}尝试获取许可", num);
-//                    semaphore.acquire();
-                log.info("线程{}-put", num);
-                lockExample3.put(num, new Data());
-                log.info("getAllKeys={}", lockExample3.getAllKeys());
-//                    log.info("线程{}-get", num);
-//                    lockExample3.get(num);
-//                    semaphore.release();
+                try {
+                    /**
+                     * //如果没有的话，那么报错为: java.lang.IllegalMonitorStateException:
+                     * attempt to unlock read lock, not locked by current thread
+                     *
+                     * 就是同时做读写操作的时候，很容易出现问题，如果是只读则没关系
+                     */
+                    log.info("线程{}尝试获取许可", num);
+                    semaphore.acquire();
+                    log.info("线程{}-put", num);
+                    lockExample3.put(num, new Data());
+                    log.info("getAllKeys={}", lockExample3.getAllKeys());
+                    log.info("线程{}-get", num);
+                    lockExample3.get(num);
+                    semaphore.release();
 
-                /* catch (InterruptedException e) {
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }*/
+                }
             });
-//            countDownLatch.countDown();
+            countDownLatch.countDown();
         }
-//        countDownLatch.await();
-//        log.info("getCount={}", countDownLatch.getCount());
+        countDownLatch.await();
+        log.info("getCount={}", countDownLatch.getCount());
         Thread.sleep(3000);
         executorService.shutdown();
         log.info("isShutdown={}", executorService.isShutdown());// true
